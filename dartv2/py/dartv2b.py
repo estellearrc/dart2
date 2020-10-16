3aebf3e520fd9c2981432d7b34c756bb17729112
hugoPiq <piquardhugo@yahoo.fr>
2020-10-16 17:06:47 +0200

    Q4

--- dartv2/py/dartv2b.py
+++ dartv2/py/dartv2b.py
@@ -1,6 +1,20 @@
 import drivers.dartv2b_basis
 import sys
 import time
+import numpy as np
+
+
+def bin2dec(X):
+    """
+    X is an array
+    """
+    for i  in range(np.shape(X)[1]):
+        if X[0,i] > 32767:
+            X[0,i] = X[0,i] -65536
+    return X
+
+
+
 
 class DartV2(drivers.dartv2b_basis.DartV2Basis):
     def __init__(self):
@@ -9,7 +23,21 @@ class DartV2(drivers.dartv2b_basis.DartV2Basis):
         super().__init__()
         # define class variables
         self.myVarDummy = 42
+    
+    def delta_front_odometers(self,dt):
+        """
+        return diff encoder wheel between dt
+        """
+        nb_ticks1 = np.array([self.get_front_encoders()])
+        time.sleep(dt)
+        nb_ticks2 = np.array([self.get_front_encoders()])
+        conv_nb_ticks1 = bin2dec(nb_ticks1)
+        conv_nb_ticks2 = bin2dec(nb_ticks2)
+        diff = conv_nb_ticks2 - conv_nb_ticks1
+        return diff
 
+    
         # place your new class variables here
 
     # place your new functions here
+
--- dartv2/py/robot_cmd_1.py
+++ dartv2/py/robot_cmd_1.py
@@ -1,9 +1,45 @@
 import dartv2b
+import time
+
+
+"""
+Notes:
+to do a turn: 2.5s
+"""
+
+
+def test_Q4(mybot,delta_t):
+    t0 = time.time()
+    while (time.time()-t0 < delta_t):
+        mybot.set_speed(100,100)
+    delta_t_turn = 2.5
+    t0 = time.time()
+    while (time.time()-t0 < delta_t_turn):
+        mybot.set_speed(100,-100)
+    t0 = time.time()
+    while (time.time()-t0 < delta_t):
+        mybot.set_speed(-100,-100)
+    delta_t_turn = 2.5
+    t0 = time.time()
+    while (time.time()-t0 < delta_t_turn):
+        mybot.set_speed(100,-100)
+
+
+def main():
+    mybot = dartv2b.DartV2()
+    t0 = time.time()
+    dt = 0.1
+    delta_t = 5
+    # while (time.time()-t0 < delta_t):
+    #     mybot.set_speed(100,-100)
+    #     # diff = mybot.delta_front_odometers(dt)
+    #     print("time: ",time.time()-t0)
+    test_Q4(mybot,delta_t)
+    mybot.end()
+
+    
 
 if __name__ == "__main__":
-    mybot = dartv2b.DartV2()
-
-    # place your work here
-
-    mybot.end() # clean end of the robot mission
+    main()
+    
 
